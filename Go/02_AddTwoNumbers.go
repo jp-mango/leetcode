49fb6132131781bb8e2bc7fbbfcbbac1e4cfead8
mango <menziesjustin@ymail.com>
2024-02-13 10:30:07 -0500

    working towards answer

--- Go/02_AddTwoNumbers.go
+++ Go/02_AddTwoNumbers.go
@@ -1,3 +1,16 @@
 package main
 
-func AddTwoNumbers() {}
+//! Definition for singly-linked list
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//* iterate through the list
+//* add each value to its corresponding index: l1[i]+l2[i]
+//* reverse the output
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l3 := ListNode{}
+	return l3.Next
+}
