98054f6d987327ee6f0f434e7b6cd1d9c396a03e
mango <menziesjustin@ymail.com>
2024-02-13 10:30:26 -0500

    cleaned up formatting

--- Go/main.go
+++ Go/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	fmt.Println("--- 01. ---")
+	fmt.Println("- 01. Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.")
 	nums := []int{2, 7, 11, 15}
+	fmt.Println("Nums:", nums)
 	target := 9
+	fmt.Println("Target:", target)
 	result := TwoSum(nums, target)
-	fmt.Printf("[%v]", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(result)), ","), "[]"))
-	fmt.Println()
+	fmt.Printf("Result: %v\n", result)
+
+	fmt.Println("\n- 02. You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.")
+
 }
