bf5c21b7e7125049281651687e4eadcbfc836414
mango <menziesjustin@ymail.com>
2024-02-13 01:13:42 -0500

    - formatted the print output

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
+	fmt.Println("--- 01. ---")
 	nums := []int{2, 7, 11, 15}
 	target := 9
-	TwoSum(nums, target)
+	result := TwoSum(nums, target)
+	fmt.Printf("[%v]", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(result)), ","), "[]"))
+	fmt.Println("\n")
 }
